cbc8abfa950e42f5892517351db80a669e2c1d79
Dhrumil <sdhrumil@gmail.com>
2018-11-19 11:22:21 +0530

    seound commit

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,12 +2,17 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class HelloController {
 
+	 private Logger logger = LoggerFactory.getLogger(this.getClass());
+	 
 	@RequestMapping("/")
 	public String sayHello(){
+		logger.info("Hello Sleuth");
 		return "Hello Dhrumil shah";
 	}
 	
